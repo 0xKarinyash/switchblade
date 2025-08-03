c504155fc505a3c3c40d6cce5affd507e128c827
0xKarinyash <karinyash96@gmail.com>
2025-08-03 10:53:42 +0400

    Fixed mutex error

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ DWORD main(void) {
     HANDLE hMutex = CreateMutexExW(NULL, MUTEX_GUID, 0, (DWORD)NULL);
     if (hMutex == NULL) {
         const DWORD err = GetLastError();
-        if (err == ERROR_ALREADY_EXISTS) {
+        if (err == ERROR_ALREADY_EXISTS || err == ERROR_ACCESS_DENIED) {
             MessageBoxExW(NULL,"Another instance of "NAME" is already running!", NULL, MB_OK | MB_ICONERROR, LANG_ENGLISH);
         } else {
             MessageBoxExW(NULL,L"Something went wrong :C", NULL, MB_OK | MB_ICONERROR, LANG_ENGLISH);
